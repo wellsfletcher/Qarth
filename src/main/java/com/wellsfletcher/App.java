3f876d40e2c778ffefdb8f78cfee46f11520c53b
Fletcher Wells <wellsfletcher@gmail.com>
2020-06-16 17:10:12 -0400

    Successfully added test file.

--- src/main/java/com/wellsfletcher/App.java
+++ src/main/java/com/wellsfletcher/App.java
@@ -1,13 +1,32 @@
 package com.wellsfletcher;
 
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
+import java.io.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+        ByteArrayOutputStream bout = QRCode.from("https://memorynotfound.com")
+            .withSize(250, 250)
+            .to(ImageType.PNG)
+            .stream();
+        try {
+            OutputStream out = new FileOutputStream("/Users/wellsfletcher/Documents/OtherCode/Java/Ouputs/qrcode.png");
+            bout.writeTo(out);
+            out.flush();
+            out.close();
+            System.out.println("Success!");
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finished.");
     }
 }
